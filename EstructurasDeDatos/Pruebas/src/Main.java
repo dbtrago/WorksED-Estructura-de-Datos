7bf94e03230f4904e2737d1d00d2695d8cfc4623
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-03-31 00:42:27 -0500

    El metodo de la IP ya funciona

--- EstructurasDeDatos/Pruebas/src/Main.java
+++ EstructurasDeDatos/Pruebas/src/Main.java
@@ -1,8 +1,12 @@
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 public class Main
 {
+    /* 
     public static void main(String[] args)
     {
         try
@@ -13,6 +17,35 @@ public class Main
         catch (UnknownHostException e)
         {
             System.out.println("No se pudo obtener la dirección IP de la máquina.");
+            e.printStackTrace();
+        }
+    */
+    public static void main(String[] args)
+    {
+        try
+        {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+
+            while (interfaces.hasMoreElements()) 
+            {
+                NetworkInterface interfaz = interfaces.nextElement();
+                if (interfaz.isLoopback() || !interfaz.isUp()) {
+                    continue;
+                }
+
+                Enumeration<InetAddress> direcciones = interfaz.getInetAddresses();
+                while (direcciones.hasMoreElements()) {
+                    InetAddress direccion = direcciones.nextElement();
+                    if (direccion.isLinkLocalAddress() || direccion.isLoopbackAddress() || direccion.isMulticastAddress()) {
+                        continue;
+                    }
+                    System.out.println("La dirección IP de la red es: " + direccion.getHostAddress());
+                }
+            }
+        } 
+        catch (SocketException e)
+        {
+            System.out.println("No se pudo obtener la dirección IP de la red.");
             e.printStackTrace();
         }
     }
