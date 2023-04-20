cc95d1e0c873d92f4c65842d5fdc1b79f61d3fcc
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2023-04-19 23:55:45 -0500

    Se agregaron cambios

--- Appointment/Server/src/Model/ServerModel.java
+++ Appointment/Server/src/Model/ServerModel.java
@@ -3,8 +3,15 @@ package Model;
 import domain.Customer;
 
 public class ServerModel {
+    public ServerModel()
+    {
+        
+    }
     public int  ValidationCustomer(Customer customer) {
         return 0;
+    }
+    public boolean deployList(MonitorAdapter monitorAdapter){
+        return false;
     }
     
 }
---
+++ Appointment/Server/src/domain/Chair.java
@@ -0,0 +1,6 @@
+package domain;
+
+public class Chair {
+    private boolean status;
+    private int id;
+}
